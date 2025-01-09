22df6ab1602948399011062fe490679835546fa7
vaio sensen <fz_bhu@163.com>
2025-01-09 12:57:49 +0800

    仅保留阿里，然后暂时去掉了推理模型。

--- app/constant.ts
+++ app/constant.ts
@@ -411,7 +411,6 @@ const alibabaModes = [
   "qwen-max",
   "qwen2.5-math-72b-instruct",
   "qwen-math-plus",
-  "qvq-72b-preview",
   "qwen-max-longcontext",
 ];
 
@@ -461,17 +460,17 @@ const chatglmModels = [
 
 let seq = 1000; // 内置的模型序号生成器从1000开始
 export const DEFAULT_MODELS = [
-  ...openaiModels.map((name) => ({
-    name,
-    available: true,
-    sorted: seq++, // Global sequence sort(index)
-    provider: {
-      id: "openai",
-      providerName: "OpenAI",
-      providerType: "openai",
-      sorted: 1, // 这里是固定的，确保顺序与之前内置的版本一致
-    },
-  })),
+  // ...openaiModels.map((name) => ({
+  //   name,
+  //   available: true,
+  //   sorted: seq++, // Global sequence sort(index)
+  //   provider: {
+  //     id: "openai",
+  //     providerName: "OpenAI",
+  //     providerType: "openai",
+  //     sorted: 1, // 这里是固定的，确保顺序与之前内置的版本一致
+  //   },
+  // })),
   // ...openaiModels.map((name) => ({
   //   name,
   //   available: true,
